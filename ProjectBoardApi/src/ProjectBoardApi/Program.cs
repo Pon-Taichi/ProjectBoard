5b2074680dd47b5eb69a5ee2860beb0cfd77fcec
Pon-Taichi <saramanda164@gmail.com>
2023-08-03 00:45:38 +0900

    create swagger.json at build

--- ProjectBoardApi/src/ProjectBoardApi/Program.cs
+++ ProjectBoardApi/src/ProjectBoardApi/Program.cs
@@ -1,7 +1,28 @@
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddAWSLambdaHosting(LambdaEventSource.RestApi);
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+  app.UseSwagger();
+  app.UseSwaggerUI();
+}
 
 app.MapGet("/", () => "Hello World!");
 
+app.MapGet("/projects", () =>
+{
+  return TypedResults.Ok(
+    new List<Project>
+    {
+      new Project("1", "sample"),
+      new Project("2", "sample2")
+    });
+}).WithTags("Projects");
+
 app.Run();
+
+record Project(string id, string name);
+
